5bcee1f6c2a7b8906b189e84e10ba1d448213df8
ansible lab <meorkamameorsulaiman@gmail.com>
2025-12-29 15:55:00 +0000

    Update start script

--- roles/jenkins/files/start-agent.sh
+++ roles/jenkins/files/start-agent.sh
@@ -2,5 +2,5 @@
 cd /usr/local/jenkins-service
 # Just in case we would have upgraded the controller, we need to make sure that the agent is using the latest version of the agent.jar
 curl -sO http://my_ip:8080/jnlpJars/agent.jar
-java -jar agent.jar -jnlpUrl http://my_ip:8080/computer/My%20New%20Ubuntu%2022%2E04%20Node%20with%20Java%20and%20Docker%20installed/jenkins-agent.jnlp -secret my_secret -workDir "/home/ansible"
+java -jar agent.jar -url http://my_ip:8080/ -secret my_secret -name "my_agent_name" -webSocket -workDir "/home/ansible"
 exit 0
