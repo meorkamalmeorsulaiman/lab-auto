2e39231dffab9b67c2bb1e617e6ec3938d687eec
kmlslaiman97@gmail.com <Ansible Lab>
2026-01-27 06:47:45 +0000

    Update ssh clean sccript

--- scripts/clean-ssh-k8s.sh
+++ scripts/clean-ssh-k8s.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.10'
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.11'
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.12'
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.13'
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.21'
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.22'
-ssh-keygen -f '/home/ansible/.ssh/known_hosts' -R '192.168.101.23'
+# List of servers (IP addresses or hostnames)
+SERVERS=(
+
+	"ctrl01" 
+	"ctrl02"
+	"ctrl03"
+	"wrk01"
+	"wrk02"
+	"wrk03"
+
+)
+
+
+echo "Cleaning up SSH known host for K8s..."
+
+for SERVER in "${SERVERS[@]}"; do
+    echo "Deleting known host for $SERVER..."
+    ssh-keygen -f "/home/ansible/.ssh/known_hosts" -R "$SERVER" 
+done
+
+
+
